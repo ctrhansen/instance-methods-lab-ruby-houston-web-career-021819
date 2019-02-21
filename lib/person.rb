ddd69c7fd04b6ce3779b0ee3aba6b246498b0f26
Thomas Hansen <ctrhansen@yahoo.com>
2019-02-21 03:37:44 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,11 @@
 class Person
   
-  def #talk
-    return "The person is talking"
+  def talk
+    "The person is talking"
   end 
   
-  def #walk 
-    return "The person is walking"
+  def walk 
+    "The person is walking"
   end  
     
 end 
